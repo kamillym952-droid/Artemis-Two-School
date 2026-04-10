cda34ed6adbc63c7160d19e21be8bb5728924bf0
kamillym952-droid <kamillym952@gmail.com>
2026-04-10 16:07:59 -0300

    criação da classe aluno e pasta models

---
+++ controle-frequencia/src/main/java/com/senai/backend/controle_frequencia/models/alunos.java
@@ -0,0 +1,44 @@
+package com.senai.backend.controle_frequencia.models;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "alunos") 
+public class alunos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+
+    @Column(unique = true)
+    private String email;
+
+    public alunos() {
+    }
+
+    public alunos(Long id, String nome, Date dataNascimento, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Date getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(Date dataNascimento) { this.dataNascimento = dataNascimento; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
